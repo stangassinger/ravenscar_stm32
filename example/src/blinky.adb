0c86d297a6c3093ddf35efe5c3fdfe5ba93c4a78
guenther <guenther.stangassinger@gmx.de>
2019-12-24 16:52:28 +0000

    ..

---
+++ example/src/blinky.adb
@@ -0,0 +1,21 @@
+with STM32.Board;   use STM32.Board;
+
+with STM32.GPIO;    use STM32.GPIO;
+with Ada.Real_Time; use Ada.Real_Time;
+
+procedure Blinky is
+
+   Period : constant Time_Span := Milliseconds (200);  -- arbitrary
+
+   Next_Release : Time := Clock;
+
+begin
+   STM32.Board.Initialize_LEDs;
+
+   loop
+      Toggle (All_LEDs);
+
+      Next_Release := Next_Release + Period;
+      delay until Next_Release;
+   end loop;
+end Blinky;
