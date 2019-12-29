7cb44f5f05b0104d7cad39662bb3dd80d638ff69
guenther stangassinger <guenther.stangassinger@gmx.de>
2019-12-29 18:50:04 +0000

    not working

--- example/src/blinky.adb
+++ example/src/blinky.adb
@@ -1,21 +1,21 @@
 --  with STM32.Board;   use STM32.Board;
 
 --  with STM32.GPIO;    use STM32.GPIO;
-with Ada.Real_Time; use Ada.Real_Time;
+--with Ada.Real_Time; use Ada.Real_Time;
 with Ada.Text_IO;
 procedure Blinky is
 
-     Period : constant Time_Span := Milliseconds (200);  -- arbitrary
+--     Period : constant Time_Span := Milliseconds (200);  -- arbitrary
 --
-     Next_Release : Time := Clock;
+--     Next_Release : Time := Clock;
 
 begin
 --   STM32.Board.Initialize_LEDs;
-
+Ada.Text_IO.put_line("xxxxxxxxx");
    loop
      -- Toggle (All_LEDs);
-      Ada.Text_IO.put_line("fwef");
-        Next_Release := Next_Release + Period;
-        delay until Next_Release;
+      Ada.Text_IO.put_line("YYYYYYYYYYYYYYYYYYYYYYYYYYY");
+       -- Next_Release := Next_Release + Period;
+        --delay until Next_Release;
    end loop;
 end Blinky;
