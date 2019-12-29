ae7de5fb0688cb265a8b1b084f3624bcc459ef0a
guenther stangassinger <guenther.stangassinger@gmx.de>
2019-12-29 17:28:23 +0000

    compiles with RTS

--- example/src/blinky.adb
+++ example/src/blinky.adb
@@ -1,21 +1,21 @@
-with STM32.Board;   use STM32.Board;
+--  with STM32.Board;   use STM32.Board;
 
-with STM32.GPIO;    use STM32.GPIO;
+--  with STM32.GPIO;    use STM32.GPIO;
 with Ada.Real_Time; use Ada.Real_Time;
-
+with Ada.Text_IO;
 procedure Blinky is
 
-   Period : constant Time_Span := Milliseconds (200);  -- arbitrary
-
-   Next_Release : Time := Clock;
+     Period : constant Time_Span := Milliseconds (200);  -- arbitrary
+--
+     Next_Release : Time := Clock;
 
 begin
-   STM32.Board.Initialize_LEDs;
+--   STM32.Board.Initialize_LEDs;
 
    loop
-      Toggle (All_LEDs);
-
-      Next_Release := Next_Release + Period;
-      delay until Next_Release;
+     -- Toggle (All_LEDs);
+      Ada.Text_IO.put_line("fwef");
+        Next_Release := Next_Release + Period;
+        delay until Next_Release;
    end loop;
 end Blinky;
