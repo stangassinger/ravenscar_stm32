2b12768b3e337c5f5d9956b8ae3eaf8a7c90cc34
guenther stangassinger <guenther.stangassinger@gmx.de>
2020-01-06 19:01:07 +0000

    compiling :-)  clock not correct;  next step is tasking example

--- example/src/blinky.adb
+++ example/src/blinky.adb
@@ -1,7 +1,7 @@
 --  with STM32.Board;   use STM32.Board;
 
 --  with STM32.GPIO;    use STM32.GPIO;
---with Ada.Real_Time; use Ada.Real_Time;
+--  with Ada.Real_Time; use Ada.Real_Time;
 with Ada.Text_IO;
 procedure Blinky is
 
@@ -11,11 +11,11 @@ procedure Blinky is
 
 begin
 --   STM32.Board.Initialize_LEDs;
-Ada.Text_IO.put_line("xxxxxxxxx");
+   Ada.Text_IO.Put_Line ("xxxxxxxxx");
    loop
-     -- Toggle (All_LEDs);
-      Ada.Text_IO.put_line("YYYYYYYYYYYYYYYYYYYYYYYYYYY");
-       -- Next_Release := Next_Release + Period;
-        --delay until Next_Release;
+      --  Toggle (All_LEDs);
+      Ada.Text_IO.Put_Line ("YYYYYYYYYYYYYYYYYYYYYYYYYYY");
+      --  Next_Release := Next_Release + Period;
+      --  delay until Next_Release;
    end loop;
 end Blinky;
