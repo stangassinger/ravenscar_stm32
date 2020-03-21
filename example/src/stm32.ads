49edcf9699b020270c10c9d052e70044714e7129
guenther stangassinger <guenther.stangassinger@gmx.de>
2020-03-21 18:33:06 +0000

    adding pragma Preelaborate in stm32.ads

--- example/src/stm32.ads
+++ example/src/stm32.ads
@@ -1,17 +1,17 @@
 --  Root package for the drivers of the STM32 family of MCU
 
 pragma Warnings (Off);
---  with Interfaces;               use Interfaces;
+with Interfaces.STM32;               use Interfaces.STM32;
 --  with HAL;     use HAL;
 pragma Warnings (On);
 
 package STM32 is
-   pragma Pure;
+   pragma Preelaborate;
 
---     type GPIO_Alternate_Function is private;
+   type GPIO_Alternate_Function is private;
 
 private
 
---     type GPIO_Alternate_Function is new UInt4;
+   type GPIO_Alternate_Function is new UInt4;
 
 end STM32;
